f5acc0dcbfc9cc934d319094dbbee828b1b7b05e
rachit2 <rachitverma.001@gmail.com>
2019-06-21 01:15:50 +0530

    Update ride.rb

--- OlaApp3/app/models/ride.rb
+++ OlaApp3/app/models/ride.rb
@@ -1,6 +1,21 @@
 class Ride < ApplicationRecord
-	has_one :Coupon
-	has_one :Rate
-	belongs_to :User
-	belongs_to :Shift
+	# has_one :Coupon
+	# has_one :Rate
+	
+	# belongs_to :Shift
+ 	
+
+	has_one :Payment
+
+ 	belongs_to :User
+	belongs_to :DriverDetail
+	belongs_to :Coupon
+
+
+	#Ride:-
+	#id user_id driver_id coupon_id distance pickup drop wait_time start_time end_time
+
+	#Coupon:-
+	#id type
+
 end
