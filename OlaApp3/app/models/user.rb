88c5fa1ae10882fb45a955042b807a3c0c3711f5
rachit2 <rachitverma.001@gmail.com>
2019-06-21 01:17:45 +0530

    Update user.rb

--- OlaApp3/app/models/user.rb
+++ OlaApp3/app/models/user.rb
@@ -1,9 +1,21 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_one :Ride
-  has_many :Role
-  has_and_belongs_to_many :DriverDetail
+
+  has_and_belongs_to_many :Role
+  has_one :DriverDetail
+  has_many :Ride
+
+  #user:-
+  #id name email mobile address password 
+
+  #ride:-
+   #id user_id driver_id coupon_id distance pickup drop wait_time 
+
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+
 end
