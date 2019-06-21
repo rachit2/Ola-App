3b3908be49057c812d5eaa2b4556dd0e45deefb0
rachit2 <rachitverma.001@gmail.com>
2019-06-21 10:42:18 +0530

    Update payment.rb

--- OlaApp3/app/models/payment.rb
+++ OlaApp3/app/models/payment.rb
@@ -1,7 +1,14 @@
 class Payment < ApplicationRecord
 
 
- belongs_to :Ride
-#Payment:-
+ #belongs_to :Ride
+
+ belongs_to :CustomerPass
+
+#Payment For Ride:-
 	#id driver_id user_id rate/km driver_charge cancel_charge Total_payment Payment_status
+
+#Payment for customerPass:-
+	#id customer_pass_id cardNumber rate/km driver_charge cancel_charge payment_status Total_amount_paid 
+
 end
